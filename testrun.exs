21b78f9bacda2f9aa515ff3ef7a027e9ef5d65e5
Emilio Nyaray <emilio@nyaray.com>
2019-05-31 16:41:06 +0200

    format testrun.exs

--- testrun.exs
+++ testrun.exs
@@ -1,58 +1,74 @@
 state = Inky.setup(nil, :phat, :red)
-state = Enum.reduce(0..(state.height - 1), state, fn y, state ->Enum.reduce(0..(state.width - 1), state, fn x, state ->Inky.set_pixel(state, x, y, state.white)end)end)
+
+state =
+  Enum.reduce(0..(state.height - 1), state, fn y, state ->
+    Enum.reduce(0..(state.width - 1), state, fn x, state ->
+      Inky.set_pixel(state, x, y, state.white)
+    end)
+  end)
+
 Inky.show(state)
 
 state = Inky.setup(nil, :phat, :red)
-state = Enum.reduce(0..(state.height - 1), state, fn y, state ->
-    Enum.reduce(0..(state.width - 1), state, fn x, state ->
-        color = state.white
-        if x < 32 do
-            color = state.red
-        end
 
-        if x < 16 do
-            color = state.black
-        end
-        # color = case x do
-        #     0 -> state.black
-        #     1 -> state.black
-        #     2 -> state.black
-        #     3 -> state.black
-        #     4 -> state.red
-        #     5 -> state.red
-        #     6 -> state.red
-        #     7 -> state.red
-        #     _ -> state.white
-        # end
-        Inky.set_pixel(state, x, y, color)
+state =
+  Enum.reduce(0..(state.height - 1), state, fn y, state ->
+    Enum.reduce(0..(state.width - 1), state, fn x, state ->
+      color = state.white
+
+      if x < 32 do
+        color = state.red
+      end
+
+      if x < 16 do
+        color = state.black
+      end
+
+      # color = case x do
+      #     0 -> state.black
+      #     1 -> state.black
+      #     2 -> state.black
+      #     3 -> state.black
+      #     4 -> state.red
+      #     5 -> state.red
+      #     6 -> state.red
+      #     7 -> state.red
+      #     _ -> state.white
+      # end
+      Inky.set_pixel(state, x, y, color)
     end)
-end)
+  end)
+
 Inky.show(state)
 
-
 Enum.reduce(
-    0..(state.height - 1),
-    state,
-    fn y, state ->
-        Enum.reduce(
-            0..(state.width - 1),
-            state,
-            fn x, state ->
-                IO.inspect({x, y})
-                state
-            end
-        )
-    end
+  0..(state.height - 1),
+  state,
+  fn y, state ->
+    Enum.reduce(
+      0..(state.width - 1),
+      state,
+      fn x, state ->
+        IO.inspect({x, y})
+        state
+      end
+    )
+  end
 )
 
-grid = Enum.reduce(0..(state.height-1), "", fn y, grid ->
-        row = Enum.reduce(0..(state.width-1), "", fn x, row ->
-                color_value = Map.get(state.pixels, {x, y}, 0)
-                row <> case color_value do
-                    0 -> "W"
-                    1 -> "B"
-                    2 -> "R"
-                end
-        end)
-        grid <> row <> "\n"
-end)
+grid =
+  Enum.reduce(0..(state.height - 1), "", fn y, grid ->
+    row =
+      Enum.reduce(0..(state.width - 1), "", fn x, row ->
+        color_value = Map.get(state.pixels, {x, y}, 0)
+
+        row <>
+          case color_value do
+            0 -> "W"
+            1 -> "B"
+            2 -> "R"
+          end
+      end)
+
+    grid <> row <> "\n"
+  end)
