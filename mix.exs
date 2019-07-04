697f707f958317dec425a1700503412178564a4b
Lars Wikman <lars.wikman@gmail.com>
2019-07-04 05:56:33 +0200

    Updated version to 1.0.0 in mix.exs.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Inky.MixProject do
   def project do
     [
       app: :inky,
-      version: "0.0.1",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/pappersverk/inky/",
